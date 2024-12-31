56554a179d7e48dd0aad0e2c3794dd34c5ab1cd3
Ryomi <ryomi.j78@gmail.com>
2024-12-31 14:53:28 +0900

    fix build error - remove return value in deleteInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -99,13 +99,14 @@ export const updateInvoice = async (
   redirect("/dashboard/invoices");
 };
 
-export const deleteInvoice = async (id: string, formData: FormData) => {
+export const deleteInvoice = async (id: string) => {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
 
-    return { message: "Deleted Invoice." };
+    // return { message: "Deleted Invoice." };
   } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice." };
+    throw new Error("Database Error: Failed to Delete Invoice.");
+    // return { message: "Database Error: Failed to Delete Invoice." };
   }
 };
